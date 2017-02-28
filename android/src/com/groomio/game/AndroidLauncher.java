fde4e51f8cf659825d3b8748c19956aa3ce2e26b
CommonBob <fabuan127@gmail.com>
2017-02-28 14:11:41 +0100

    Pierwsze dodanie

---
+++ android/src/com/groomio/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.groomio.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.groomio.game.Groomio;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Groomio(), config);
+	}
+}
